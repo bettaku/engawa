c8c894b866dc6b7747636329877c2b4b875ddfa6
pen <121443048+penginn-0@users.noreply.github.com>
2024-03-25 13:56:51 +0900

    lint?

--- packages/backend/src/core/SearchService.ts
+++ packages/backend/src/core/SearchService.ts
@@ -242,11 +242,9 @@ export class SearchService {
 					query.andWhere('user.host = :host', { host: opts.host });
 				}
 			}
-			if(opts.fileOption){
-				if(opts.fileOption === 'file-only'){
-					
-				}if(opts.fileOption === 'no-file'){
-					
+			if (opts.fileOption) {
+				if (opts.fileOption === 'file-only') {
+				} if (opts.fileOption === 'no-file') {
 				}
 			}
 			this.queryService.generateVisibilityQuery(query, me);
