c55657b0cddbb240795750df768d9140604c9439
Esurio <esurio@esurio1673.net>
2024-08-06 13:07:03 +0000

    wip: DBモデル

---
+++ packages/backend/src/models/ScheduledNote.ts
@@ -0,0 +1,43 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { Entity, Index, JoinColumn, Column, PrimaryColumn, ManyToOne } from 'typeorm';
+import { noteVisibilities } from '@/types.js';
+import { id } from './util/id.js';
+import { MiUser } from './User.js';
+import { MiNote } from './Note.js';
+
+@Entity('scheduled_note')
+export class MiScheduledNote {
+	@PrimaryColumn(id())
+	public id: string;
+
+	@Index()
+	@Column({
+		...id(),
+		comment: 'The ID of the note.',
+	})
+	public noteId: MiNote['id'];
+
+	@Index()
+	@Column(id())
+	public userId: MiUser['id'];
+
+	@Column('timestamp with time zone', {
+		comment: 'The time when the note will be published.',
+	})
+	public scheduledAt: Date;
+
+	@ManyToOne(() => MiNote, {
+		onDelete: 'CASCADE',
+	})
+	@JoinColumn()
+	public note: MiNote;
+
+	@ManyToOne(() => MiUser, {
+		onDelete: 'CASCADE',
+	})
+	public user: MiUser;
+}
