7f7ee545bcab3f04ec76b5ceebce37eafbe11558
Esurio <esurio@esurio1673.net>
2024-08-20 17:49:47 +0000

    fix: reactionHost

--- packages/frontend/src/components/MkReactionsViewer.reaction.vue
+++ packages/frontend/src/components/MkReactionsViewer.reaction.vue
@@ -68,7 +68,8 @@ const reactionName = computed(() => {
 
 const reactionHost = computed(() => {
 	const r = props.reaction.replace(':', '');
-	return r.slice(r.indexOf('@') + 1);
+	const host = r.slice(r.indexOf('@') + 1);
+	return host.endsWith(':') ? host.slice(0, -1): host;
 });
 
 const alternative: ComputedRef<string | null> = computed(() => defaultStore.state.reactableRemoteReactionEnabled ? (customEmojis.value.find(it => it.name === reactionName.value)?.name ?? null) : null);
