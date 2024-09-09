4d0cf98145562cbbc858cca05f60b34a4de3cf71
Esurio <esurio@esurio1673.net>
2024-09-09 14:23:37 +0000

    NOT NULL制約消す

---
+++ packages/backend/migration/1725891731600-SearchbleByNull.js
@@ -0,0 +1,13 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class SearchbleByNull1725891731600 {
+    name = 'SearchbleByNull1725891731600'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" DROP NOT NULL`);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" SET NOT NULL`);
+    }
+}
