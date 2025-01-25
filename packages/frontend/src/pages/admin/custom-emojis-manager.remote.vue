00bd19a42424f00cc45526f880d86daff5efa6da
Esurio <esurio@esurio1673.net>
2025-01-25 13:30:37 +0000

    Revert "enhance: 絵文字のグリッドUIでリモートページからライセンスを確認できるように"
    This reverts commit 0f657f24385b009ad915a4cac2fd8d9a8bd4e73b.
    upstreamとconflictする可能性があるので

--- packages/frontend/src/pages/admin/custom-emojis-manager.remote.vue
+++ packages/frontend/src/pages/admin/custom-emojis-manager.remote.vue
@@ -143,7 +143,6 @@ type GridItem = {
 	url: string;
 	name: string;
 	host: string;
-	license: string;
 }
 
 function setupGrid(): GridSetting {
@@ -181,7 +180,6 @@ function setupGrid(): GridSetting {
 			{ bindTo: 'host', title: 'host', type: 'text', editable: false, width: 'auto' },
 			{ bindTo: 'uri', title: 'uri', type: 'text', editable: false, width: 'auto' },
 			{ bindTo: 'publicUrl', title: 'publicUrl', type: 'text', editable: false, width: 'auto' },
-			{ bindTo: 'license', title: 'license', type: 'text', editable: false, width: 'auto' },
 		],
 		cells: {
 			contextMenuFactory: (col, row, value, context) => {
@@ -333,7 +331,6 @@ async function refreshCustomEmojis() {
 		url: it.publicUrl,
 		name: it.name,
 		host: it.host!,
-		license: it.license!,
 	}));
 }
 
