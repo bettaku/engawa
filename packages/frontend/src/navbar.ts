739ace7680f99facfa98a50b3a4a2dbd40a128f7
Esurio <rekishinomori@gmail.com>
2025-02-15 02:24:22 +0900

    chore: use miLocalStorage

--- packages/frontend/src/navbar.ts
+++ packages/frontend/src/navbar.ts
@@ -139,7 +139,7 @@ export const navbarItemDef = reactive({
 				text: 'Friendly',
 				active: ui === 'friendly' || ui === null,
 				action: () => {
-					localStorage.setItem('ui', 'friendly');
+					miLocalStorage.setItem('ui', 'friendly');
 					unisonReload();
 				},
 			}, {
