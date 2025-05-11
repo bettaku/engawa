8b206fd1584dfaa4a857d67afbbeaf86707506f2
Esurio <rekishinomori@gmail.com>
2025-05-12 02:05:15 +0900

    fix: 返信ボタンが誤った位置に表示される

--- packages/frontend/src/components/MkPostFormSimple.vue
+++ packages/frontend/src/components/MkPostFormSimple.vue
@@ -81,7 +81,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 		<div v-if="targetChannel" :class="$style.colorBar" :style="{ background: targetChannel.color }"></div>
 		<textarea ref="textareaEl" v-model="text" :class="[$style.text]" :disabled="posting || posted" :readonly="textAreaReadOnly" :placeholder="placeholder" data-cy-post-form-text @click="formClick" @keydown="onKeydown" @keyup="onKeyup" @paste="onPaste" @compositionupdate="onCompositionUpdate" @compositionend="onCompositionEnd"/>
 		<div v-if="maxTextLength - textLength < 100" :class="['_acrylic', $style.textCount, { [$style.textOver]: textLength > maxTextLength }]">{{ maxTextLength - textLength }}</div>
-		<div v-if="!showForm" style="position: fixed; bottom: 0; right: 8px;" :class="$style.submit">
+		<div v-if="!showForm" style="position: absolute; bottom: 0; right: 8px;" :class="$style.submit">
 			<button v-click-anime class="_button" :class="$style.submitButton" :disabled="!canPost && $i" data-cy-open-post-form-submit @click="$i ? post : signin()">
 				<div :class="$style.submitInner">
 					<template v-if="posted"></template>
