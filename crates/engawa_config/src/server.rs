81b0ae00b9c1d074525457f91e3efee352fe0d4c
Esurio <rekishinomori@gmail.com>
2025-11-08 21:04:11 +0200

    wip: config

--- crates/engawa_config/src/server.rs
+++ crates/engawa_config/src/server.rs
@@ -1,2 +1,91 @@
 // Server Configuration module
 
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BackendConfig {
+	pub url: String,
+	pub port: u64,
+	pub db: DbConfig,
+	pub redis: RedisConfig,
+	pub id: IdMethod,
+	pub search_provider: SearchProvider,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DbConfig {
+	pub host: String,
+	pub port: u64,
+	pub db: String,
+	pub user: String,
+	pub pass: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RedisConfig {
+	pub host: String,
+	pub port: u16,
+
+	#[serde(default)]
+	pub username: Option<String>,
+
+	#[serde(default)]
+	pub pass: Option<String>,
+
+	#[serde(default)]
+	pub db: Option<u8>,
+
+	#[serde(default)]
+	pub prefix: Option<String>,
+
+	#[serde(default)]
+	pub pubsub: Option<Box<RedisInstanceConfig>>,
+
+	#[serde(default)]
+	pub queue: Option<Box<RedisInstanceConfig>>,
+
+	#[serde(default)]
+	pub timeline: Option<Box<RedisInstanceConfig>>,
+
+	#[serde(default)]
+	pub reactions: Option<Box<RedisInstanceConfig>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RedisInstanceConfig {
+	pub host: String,
+	pub port: u16,
+
+	#[serde(default)]
+	pub username: Option<String>,
+
+	#[serde(default)]
+	pub pass: Option<String>,
+
+	#[serde(default)]
+	pub db: Option<u8>,
+
+	#[serde(default)]
+	pub prefix: Option<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum IdMethod {
+	Aid, // deprecated
+	Aidx,
+	Meid, // deprecated
+	Ulid,
+	Objectid, // deprecated
+	Snowflake,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum SearchProvider {
+	Postgres,
+	Meilisearch,
+	Elasticsearch
+}
