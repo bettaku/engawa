283448a439f9ac67e6a717efec8c741873a3ade6
Esurio <rekishinomori@gmail.com>
2025-12-21 21:35:47 +0200

    fix types

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -56,7 +56,7 @@ export const store = markRaw(new Pizzax('base', {
 	tl: {
 		where: 'deviceAccount',
 		default: {
-			src: 'home' as 'home' | 'local' | 'social' | 'global' | `list:${string}`,
+			src: 'home' as 'home' | 'local' | 'social' | 'global' | 'media' | 'bubble' | `list:${string}`,
 			userList: null as Misskey.entities.UserList | null,
 			filter: {
 				withReplies: false,
