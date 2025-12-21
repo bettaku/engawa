82c87c137f9ee298330c73fbb2e13855d7174f80
Esurio <rekishinomori@gmail.com>
2025-12-22 01:18:02 +0200

    fix: NoteDraftにsearchableByを追加した際のmigration忘れ

---
+++ packages/backend/migration/1766357899292-AddSearchableByForNoteDrafts.js
@@ -0,0 +1,11 @@
+export class AddSearchableByForNoteDrafts1766357899292 {
+    name = 'AddSearchableByForNoteDrafts1766357899292'
+
+    async up(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note_draft" ADD COLUMN "searchableBy" "note_searchableBy_enum" DEFAULT 'public' `);
+    }
+
+    async down(queryRunner) {
+        await queryRunner.query(`ALTER TABLE "note_draft" DROP COLUMN "searchableBy"`);
+    }
+}
