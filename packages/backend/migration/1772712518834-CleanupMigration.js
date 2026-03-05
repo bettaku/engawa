1f5d909c0707b9823f3020ed3a0a1844bb655d66
Esurio <rekishinomori@gmail.com>
2026-03-05 14:21:25 +0200

    update migration

--- packages/backend/migration/1772712518834-CleanupMigration.js
+++ packages/backend/migration/1772712518834-CleanupMigration.js
@@ -2,6 +2,8 @@ export class CleanupMigration1772712518834 {
     name = 'CleanupMigration1772712518834'
 
     async up(queryRunner) {
+        await queryRunner.query(`DROP INDEX IF EXISTS "public"."IDX_renote_muting_muteeId"`);
+        await queryRunner.query(`DROP INDEX IF EXISTS "public"."IDX_renote_muting_muterId"`);
         await queryRunner.query(`ALTER TYPE "note_searchableBy_enum" RENAME TO "note_searchableby_enum"`);
     }
 
