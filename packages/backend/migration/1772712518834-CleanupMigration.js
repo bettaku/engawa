51d97a5fc8a4b6807433edbb29f5cd86dffe54d0
Esurio <rekishinomori@gmail.com>
2026-03-05 17:05:37 +0200

    update migration

--- packages/backend/migration/1772712518834-CleanupMigration.js
+++ packages/backend/migration/1772712518834-CleanupMigration.js
@@ -7,11 +7,14 @@ export class CleanupMigration1772712518834 {
 
         await queryRunner.query(`ALTER TABLE "poll_vote" DROP CONSTRAINT IF EXISTS "FK_poll_vote_poll"`);
 
+        // note_searchableBy_enumの再作成
         await queryRunner.query(`ALTER TYPE "note_searchableBy_enum" RENAME TO "note_searchableby_enum_old"`);
         await queryRunner.query(`CREATE TYPE "note_searchableby_enum" AS ENUM('public', 'followers', 'reacted', 'limited')`);
         await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" DROP DEFAULT`);
-        await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
         await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
+        await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
+        await queryRunner.query(`ALTER TABLE "note_draft" SET DEFAULT 'public'`);
         await queryRunner.query(`DROP TYPE "public"."note_searchableby_enum_old"`);
     }
 
