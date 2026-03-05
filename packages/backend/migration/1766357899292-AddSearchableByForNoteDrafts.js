08f260fdb9e2e424509a27fe74956907f8d1c46a
Esurio <rekishinomori@gmail.com>
2026-03-05 13:32:22 +0200

    fix migration

--- packages/backend/migration/1766357899292-AddSearchableByForNoteDrafts.js
+++ packages/backend/migration/1766357899292-AddSearchableByForNoteDrafts.js
@@ -2,7 +2,7 @@ export class AddSearchableByForNoteDrafts1766357899292 {
     name = 'AddSearchableByForNoteDrafts1766357899292'
 
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "note_draft" ADD COLUMN "searchableBy" "note_searchableby_enum" DEFAULT 'public' `);
+        await queryRunner.query(`ALTER TABLE "note_draft" ADD COLUMN "searchableBy" "note_searchableBy_enum" DEFAULT 'public' `);
     }
 
     async down(queryRunner) {
