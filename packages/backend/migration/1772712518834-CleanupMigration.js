1393b7d3533d3e870beb7c19d698270c48c479b8
Esurio <rekishinomori@gmail.com>
2026-03-05 16:53:33 +0200

    typo

--- packages/backend/migration/1772712518834-CleanupMigration.js
+++ packages/backend/migration/1772712518834-CleanupMigration.js
@@ -7,7 +7,7 @@ export class CleanupMigration1772712518834 {
 
         await queryRunner.query(`ALTER TABLE "poll_vote" DROP CONSTRAINT IF EXISTS "FK_poll_vote_poll"`);
 
-        await queryRunner.query(`ALTER TYPE "note_searchableBy_enum" RENAME TO "note_searchableBy_enum_old"`);
+        await queryRunner.query(`ALTER TYPE "note_searchableBy_enum" RENAME TO "note_searchableby_enum_old"`);
         await queryRunner.query(`CREATE TYPE "note_searchableby_enum" AS ENUM('public', 'followers', 'reacted', 'limited')`);
         await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
