b09151a764b7cb835ce21fa1592d117769fbb123
Esurio <rekishinomori@gmail.com>
2026-03-05 18:02:56 +0200

    update SPDX

--- packages/backend/migration/1772712518834-CleanupMigration.js
+++ packages/backend/migration/1772712518834-CleanupMigration.js
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: syuilo and misskey-project
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 export class CleanupMigration1772712518834 {
     name = 'CleanupMigration1772712518834'
 
