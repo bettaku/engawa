6350202f9653cdcb2f6c4e428947f99dca8a466e
syuilo <4439005+syuilo@users.noreply.github.com>
2026-03-09 08:14:12 +0900

    Merge commit from fork
    Co-authored-by: Julia Johannesen <197614925+juliajohannesen@users.noreply.github.com>

--- packages/backend/src/core/ReactionService.ts
+++ packages/backend/src/core/ReactionService.ts
@@ -238,7 +238,7 @@ export class ReactionService {
 					},
 				});
 
-		this.globalEventService.publishNoteStream(note.id, 'reacted', {
+		this.globalEventService.publishNoteStream(note, 'reacted', {
 			reaction: decodedReaction.reaction,
 			emoji: customEmoji != null ? {
 				name: customEmoji.host ? `${customEmoji.name}@${customEmoji.host}` : `${customEmoji.name}@.`,
@@ -312,7 +312,7 @@ export class ReactionService {
 				.execute();
 		}
 
-		this.globalEventService.publishNoteStream(note.id, 'unreacted', {
+		this.globalEventService.publishNoteStream(note, 'unreacted', {
 			reaction: this.decodeReaction(exist.reaction).reaction,
 			userId: user.id,
 		});
