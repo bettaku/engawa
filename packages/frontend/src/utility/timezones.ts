0ee72df32e6d20dc1288f353874f03e6e3baad1f
Esurio <rekishinomori@gmail.com>
2026-08-17 00:24:26 +0300

    fix(frontend): remove unused constants and imports

--- packages/frontend/src/utility/timezones.ts
+++ packages/frontend/src/utility/timezones.ts
@@ -3,14 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { miLocalStorage } from '@/local-storage.js';
-
 export const timezones = () => {
-	const locale = miLocalStorage.getItem('lang') ?? navigator.language;
-	const formatter = new Intl.DateTimeFormat(locale, {
-		timeZoneName: 'short',
-	});
-
 	const timezone = Intl.supportedValuesOf('timeZone').map((tz) => {
 		return {
 			name: tz,
