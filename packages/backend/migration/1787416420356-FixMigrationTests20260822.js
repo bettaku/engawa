e999d5dda73efb8ae77339600d7466e931ca536e
Esurio <rekishinomori@gmail.com>
2026-08-22 19:46:24 +0300

    fix(backend): make migration clean

--- packages/backend/migration/1787416420356-FixMigrationTests20260822.js
+++ packages/backend/migration/1787416420356-FixMigrationTests20260822.js
@@ -10,9 +10,6 @@ export class FixMigrationTests202608221787416420356 {
         await queryRunner.query(`ALTER TYPE "public"."note_searchableby_enum" RENAME TO "note_searchableby_enum_old"`);
         await queryRunner.query(`CREATE TYPE "public"."note_searchableby_enum" AS ENUM('public', 'followers', 'reacted', 'limited')`);
         await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
-        await queryRunner.query(`DROP TYPE "public"."note_searchableby_enum_old"`);
-        await queryRunner.query(`ALTER TYPE "public"."note_searchableby_enum" RENAME TO "note_searchableby_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."note_searchableby_enum" AS ENUM('public', 'followers', 'reacted', 'limited')`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum" USING "searchableBy"::"text"::"public"."note_searchableby_enum"`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" SET DEFAULT 'public'`);
@@ -21,13 +18,10 @@ export class FixMigrationTests202608221787416420356 {
 
     async down(queryRunner) {
         await queryRunner.query(`CREATE TYPE "public"."note_searchableby_enum_old" AS ENUM('circle', 'followers', 'limited', 'public', 'reacted')`);
+        await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum_old" USING "searchableBy"::"text"::"public"."note_searchableby_enum_old"`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" DROP DEFAULT`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum_old" USING "searchableBy"::"text"::"public"."note_searchableby_enum_old"`);
         await queryRunner.query(`ALTER TABLE "note_draft" ALTER COLUMN "searchableBy" SET DEFAULT 'public'`);
-        await queryRunner.query(`DROP TYPE "public"."note_searchableby_enum"`);
-        await queryRunner.query(`ALTER TYPE "public"."note_searchableby_enum_old" RENAME TO "note_searchableby_enum"`);
-        await queryRunner.query(`CREATE TYPE "public"."note_searchableby_enum_old" AS ENUM('circle', 'followers', 'limited', 'public', 'reacted')`);
-        await queryRunner.query(`ALTER TABLE "note" ALTER COLUMN "searchableBy" TYPE "public"."note_searchableby_enum_old" USING "searchableBy"::"text"::"public"."note_searchableby_enum_old"`);
         await queryRunner.query(`DROP TYPE "public"."note_searchableby_enum"`);
         await queryRunner.query(`ALTER TYPE "public"."note_searchableby_enum_old" RENAME TO "note_searchableby_enum"`);
     }
