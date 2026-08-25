16f8e251ef1297c50cbe2a7d6bd7860479482c68
Esurio <rekishinomori@gmail.com>
2026-08-25 17:09:01 +0300

    fix(frontend): type error on RouterView

--- packages/frontend/src/components/global/RouterView.vue
+++ packages/frontend/src/components/global/RouterView.vue
@@ -46,8 +46,9 @@ provide(DI.routerCurrentDepth, currentDepth + 1);
 
 const rootEl = useTemplateRef('rootEl');
 onMounted(() => {
-	if (prefer.s.animation && prefer.s.smoothTransitionAnimations) {
-		rootEl.value.style.viewTransitionName = viewId; // view-transition-nameにcss varが使えないっぽいため直接代入
+	const root = rootEl.value;
+	if (prefer.s.animation && prefer.s.smoothTransitionAnimations && root != null) {
+		root.style.viewTransitionName = viewId; // view-transition-nameにcss varが使えないっぽいため直接代入
 	}
 });
 
@@ -84,23 +85,26 @@ let currentRoutePath = current.route.path;
 const key = ref(router.getCurrentFullPath());
 
 router.useListener('change', ({ resolved }) => {
-	if (resolved == null || 'redirect' in resolved.route) return;
-	if (resolved.route.path === currentRoutePath && deepEqual(resolved.props, currentPageProps.value)) return;
+	if (resolved == null) return;
+	const route = resolved.route;
+	if (!('component' in route)) return;
+	const component = route.component;
+	if (route.path === currentRoutePath && deepEqual(resolved.props, currentPageProps.value)) return;
 
 	function _() {
-		currentPageComponent.value = resolved.route.component;
+		currentPageComponent.value = component;
 		currentPageProps.value = resolved.props;
 		key.value = router.getCurrentFullPath();
-		currentRoutePath = resolved.route.path;
+		currentRoutePath = route.path;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
 	if (prefer.s.animation && prefer.s.smoothTransitionAnimations && window.document.startViewTransition) {
-		window.document.startViewTransition(() => new Promise((res) => {
+		window.document.startViewTransition(() => new Promise<void>((resolve) => {
 			_();
 			nextTick(() => {
-				res();
-				//setTimeout(res, 100);
+				resolve();
+				//setTimeout(resolve, 100);
 			});
 		}));
 	} else {
